8738d19df2619f28cbcdc04568ee88b886e9c8ed
Drew Marshburn <drewmarshburn@gmail.com>
2025-11-14 09:49:23 -0500

    add global terminal double-escape keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Terminal: Exit terminal mode with double escape (all terminal buffers)
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
 vim.keymap.set("n", "<leader>e", function()
   -- Check if a Neo-tree buffer already exists
   local neotree_bufnr = vim.fn.bufnr("neo-tree")
